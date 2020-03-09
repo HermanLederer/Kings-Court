b2138371907db85a1755bf1db7d2086cb5dc32e4
Landerack <37659414+Landerack@users.noreply.github.com>
2020-03-09 10:54:16 +0100

    RayAI_RuleDoodling

--- Assets/Scripts/AIs/RaysAI.cs
+++ Assets/Scripts/AIs/RaysAI.cs
@@ -22,9 +22,54 @@ public class RaysAI : PlayerAI
 
 	void Update()
 	{
-		
+//-------------------------------------------------------------------------------
+		//The war bringer, Nio (Assassin)
+			//check what Nio sees.
+				//if Nio sees an enemy defender, nio head in that direction.
+						//Nio will then move to pass the defender, continuously checking if they see the target.
+				//if at any time Nio sees the target, it will chase.
+				//if Nio sees an attacker, Trigger WarbringerSplit.
+				//else if Nio sees nothing, then 
+
+//-------------------------------------------------------------------------------
+
+
+
+		//Lady 'Get fucked' Amari (Defender)
+			//Circle SnuffleSnuff
+				//If Amari sees an attacker, Trigger snufflsnuff split.
+
+
+//------------------------------------------------------------------------------
+
+		//Sir Noble SnuffleSnuff (Target)
+			//Follow Nio.
+		//Check what SnuffleSnuff sees.
+			//If something is approaching SnuffleSnuff, trigger AmariStungun to stun it. then run the other way.
+			//T
+
+
+		//Snufflesnuff split
+			//run the oposite direction from Amari and hide behind a nearby corner. 
+			//then wait for amari to have registered the area to be safe.
+
+		//Warbringer split
+			//SnuffleSnuff will run and hide behind a nearby corner.
+			//Trigger Amari's GuardingStance.
+
+		//AmariStungun
+			//Overrule guardingStance, and only attack the enemy attacker.
+			//if the enemy attacker hasnt gotten closer to SnuffleSnuff in the last 3 seconds, Go back to target and circling.
+
+//-------------------------------------------------------------------------------
+
+
+
+
 	}
 	//--------------------------
 	// RaysAI methods
 	//--------------------------
+
+
 }
