62a66faec153e550c47fd69cbcb7dc2e123d51ac
Landerack <37659414+Landerack@users.noreply.github.com>
2020-03-16 23:33:33 +0100

    CasualBeginnings

--- Assets/Scripts/AIs/RaysAI.cs
+++ Assets/Scripts/AIs/RaysAI.cs
@@ -11,57 +11,104 @@ public class RaysAI : PlayerAI
 	// Public variables
 
 	// Private variables
-
+	private bool WarBringer = false;
+	private bool warbringerSplit = false;
+	private float warbringerSplitTimer = 0.0f;
 	//--------------------------
 	// MonoBehaviour methods
 	//--------------------------
 	new void Start()
 	{
 		base.Start();
+		target = Instantiate(targetPrefab, transform.position + Vector3.right * -2f, transform.rotation, transform).GetComponent<AICore.AIBrainInterface>();
+		assassin = Instantiate(assassinPrefab, transform.position, transform.rotation, transform).GetComponent<AICore.AIBrainInterface>();
+		stunner = Instantiate(stunnerPrefab, transform.position + Vector3.right * 2f, transform.rotation, transform).GetComponent<AICore.AIBrainInterface>();
 	}
 
 	void Update()
 	{
-//-------------------------------------------------------------------------------
+		//-------------------------------------------------------------------------------
 		//The war bringer, Nio (Assassin)
-			//check what Nio sees.
-				//if Nio sees an enemy defender, nio head in that direction.
-						//Nio will then move to pass the defender, continuously checking if they see the target.
-				//if at any time Nio sees the target, it will chase.
-				//if Nio sees an attacker, Trigger WarbringerSplit.
-				//else if Nio sees nothing, then 
+		//check what Nio sees.
+		//if Nio sees an enemy defender, nio head in that direction.
+		//Nio will then move to pass the defender, continuously checking if they see the target.
+		//if at any time Nio sees the target, it will chase.
+		//if Nio sees an attacker, Trigger WarbringerSplit.
+		//else if Nio sees nothing, then she'll return to roaming.
+		foreach (AICore.AIBrainInterface visibleEntityInterface in assassin.GetVisibleAIEntities())
+		{
+			if (visibleEntityInterface.team != target.team && visibleEntityInterface.type == AICore.AIType.assassin)
+			{
+				warbringerSplit = true;
+			}
+			if (visibleEntityInterface.team != target.team && visibleEntityInterface.type == AICore.AIType.target)
+			{ 
+				assassin.SetDestination(visibleEntityInterface.transform.position);
+				WarBringer = true;
+			}
+			else if ((visibleEntityInterface.team != target.team && visibleEntityInterface.type == AICore.AIType.target) && (WarBringer == false))
+			{
+				assassin.SetDestination(visibleEntityInterface.transform.position);
+			}
+			else
+			{
+				WarBringer = false;
+			}
+		}
 
-//-------------------------------------------------------------------------------
+		if (warbringerSplit == true)
+		{
+			warbringerSplitTimer += Time.deltaTime;
+		}
+		//check every second if we still see anyone.
+		if(warbringerSplitTimer >= 1f)
+		{
+			warbringerSplit = false;
+		}
+
+		//roaming isn't done yet.
+
+
+		//-------------------------------------------------------------------------------
 
 
 
 		//Lady 'Get fucked' Amari (Defender)
-			//Circle SnuffleSnuff
-				//If Amari sees an attacker, Trigger snufflsnuff split.
+		//Circle SnuffleSnuff
+		//If Amari sees an attacker, Trigger snufflsnuff split.
+
+		//Amiri's Guarding stance
+		//Amiri will patroll, in case amiri sees any attacker. she will run and stunn. Then trigger snuffleSnuffSplit will run the other way.
 
 
-//------------------------------------------------------------------------------
+		//------------------------------------------------------------------------------
 
 		//Sir Noble SnuffleSnuff (Target)
-			//Follow Nio.
+		//Follow Nio.
 		//Check what SnuffleSnuff sees.
-			//If something is approaching SnuffleSnuff, trigger AmariStungun to stun it. then run the other way.
-			//T
+		//If something is approaching SnuffleSnuff, trigger AmariStungun to stun it. then run the other way.
+		//T
 
 
-		//Snufflesnuff split
-			//run the oposite direction from Amari and hide behind a nearby corner. 
-			//then wait for amari to have registered the area to be safe.
+		//snufflesnuffSplit
+		//run the oposite direction from the attaker Amari sees and hide out of sight.
+		//then wait for amari to have registered the area to be safe.
+
 
 		//Warbringer split
+		if (warbringerSplit == true)
+		{
 			//SnuffleSnuff will run and hide behind a nearby corner.
 			//Trigger Amari's GuardingStance.
+		}
+
+
 
 		//AmariStungun
-			//Overrule guardingStance, and only attack the enemy attacker.
-			//if the enemy attacker hasnt gotten closer to SnuffleSnuff in the last 3 seconds, Go back to target and circling.
+		//Overrule guardingStance, and only attack the enemy attacker.
+		//if the enemy attacker hasnt gotten closer to SnuffleSnuff in the last 3 seconds, Go back to target and circling.
 
-//-------------------------------------------------------------------------------
+		//-------------------------------------------------------------------------------
 
 
 
