6b457e8adb584f9b91685eed7699e1e1a2afdb41
Sirradez <bosna_iamar@live.nl>
2020-03-22 17:11:53 +0100

    added "death" board

--- Assets/Scripts/AIs/AmarsAI.cs
+++ Assets/Scripts/AIs/AmarsAI.cs
@@ -10,20 +10,51 @@ public class AmarsAI : PlayerAI
 
 	// Private variables
 
+	private float enemyDistanceRun = 4.0f;
+
+	private float nextRandomMoveTime;
+
 	//--------------------------
 	// MonoBehaviour methods
 	//--------------------------
 	new void Start()
 	{
 		base.Start();
+		nextRandomMoveTime = 0f;
 	}
 
-	void Update()
+
+	private void Update()
 	{
 
-	}
-	void OnDrawGizmosSelected()
-	{
+		if (nextRandomMoveTime <= Time.time)
+		{
+			foreach (AICore.AIBrainInterface member in members)
+			{
+				member.SetDestination(member.transform.position + Vector3.one * Random.Range(-5f, 5f));
+			}
+
+			nextRandomMoveTime = Time.time + 0.5f;
+		}
+
+		// target
+		foreach (AICore.AIBrainInterface visibleEntityInterface in target.GetVisibleAIEntities())
+		{
+			if (visibleEntityInterface.team != target.team && visibleEntityInterface.team != stunner.team
+			&& visibleEntityInterface.type == AICore.AIType.assassin) target.SetDestination(-visibleEntityInterface.transform.position);
+		}
+
+		// assassin
+		foreach (AICore.AIBrainInterface visibleEntityInterface in assassin.GetVisibleAIEntities())
+		{
+			if (visibleEntityInterface.team != target.team && visibleEntityInterface.type == AICore.AIType.target) assassin.SetDestination(visibleEntityInterface.transform.position);
+		}
+
+		// stunner
+		foreach (AICore.AIBrainInterface visibleEntityInterface in stunner.GetVisibleAIEntities())
+		{
+			if (visibleEntityInterface.team != target.team && visibleEntityInterface.type != AICore.AIType.stunner) stunner.SetDestination(visibleEntityInterface.transform.position);
+		}
 
 	}
 
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+	[Space(5)]
+// -----Canvas for when a team dies-----
+        public GameObject team1Down;
+        public GameObject team2Down;
+        public GameObject team3Down;
+
+		AICore.AITeam currentTeam;
+
 	#region Singleton
 	public static GameManager instance;
 
 	void Awake()
 	{
+		currentTeam = AICore.AITeam.Amar;
 		instance = this;
 	}
 	#endregion
@@ -37,6 +46,21 @@ public class GameManager : MonoBehaviour
 			Debug.Log("Team " + team + " eliminated");
 			teams.Remove(team);
 			if (teams.Count == 1) DecalreWinnder(teams[0]);
+			
+			if (currentTeam == AICore.AITeam.Amar)
+			{
+				team1Down.SetActive(true);
+			}
+
+			if (currentTeam == AICore.AITeam.Herman)
+			{
+				team2Down.SetActive(true);
+			}
+
+			if (currentTeam == AICore.AITeam.Ray)
+			{
+				team3Down.SetActive(true);
+			}
 		}
 	}
 
