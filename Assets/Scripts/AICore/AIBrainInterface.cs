8d821005533c59678b6f49fb18beb9d85b1772f1
Herman Lederer <germans.lederers@gmail.com>
2020-03-26 14:33:07 +0200

    Implement sounds

--- Assets/Scripts/AICore/AIBrainInterface.cs
+++ Assets/Scripts/AICore/AIBrainInterface.cs
@@ -16,11 +16,13 @@ namespace AICore
 		private AIPerceptionBrain perceptionBrain;
 		private AICombatBrain combatBrain;
 		private NavMeshAgent navMeshAgent;
+		private AudioSource audioSource;
 
 		// Editor variables
 		[SerializeField] public AITeam team;
 		[SerializeField] public AIType type;
 		[SerializeField] public GameObject deathFXPrefab;
+		[SerializeField] public AudioClip stunSound;
 
 		// Private variables
 		public float stun { get; private set; }
@@ -33,6 +35,7 @@ namespace AICore
 			perceptionBrain = GetComponent<AIPerceptionBrain>();
 			combatBrain = GetComponent<AICombatBrain>();
 			navMeshAgent = GetComponent<NavMeshAgent>();
+			audioSource = GetComponent<AudioSource>();
 
 			stun = 0;
 		}
@@ -58,6 +61,7 @@ namespace AICore
 			if (transform.parent.gameObject.GetComponent<PlayerAI>().isEliminated) return;
 			SetDestination(transform.position);
 			stun = Time.time + time;
+			audioSource.PlayOneShot(stunSound);
 		}
 
 		public void SetDestination(Vector3 target)
@@ -86,17 +90,25 @@ namespace AICore
 		{
 			if (transform.parent.gameObject.GetComponent<PlayerAI>().isEliminated) return;
 
+			audioSource.PlayOneShot(stunSound);
+
 			// not receiving damage if not target
 			if (type != AIType.target) return;
 
 			Die();
 		}
 
+		/// <summary>
+		/// Kills the entire team
+		/// </summary>
 		public void Die()
 		{
 			transform.parent.gameObject.GetComponent<PlayerAI>().Eliminate();
 		}
 
+		/// <summary>
+		/// Visualizes dying on the entity
+		/// </summary>
 		public void Burn()
 		{
 			Instantiate(deathFXPrefab, transform.position, transform.rotation);
